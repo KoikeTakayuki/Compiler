c544377e3be452142f39eed5a272544f821a6f69
KoikeTakayuki <tekasax@gmail.com>
2017-01-15 14:56:30 +0900

    Initial Commit

---
+++ compiler/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  compiler
+//
+//  Created by 小池貴之 on 2017/01/15.
+//  Copyright © 2017年 小池貴之. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
